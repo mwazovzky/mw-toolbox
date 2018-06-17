06107201d4df9e72782799c3ced4b6dc6679bd6b
Mike Wazovzky <mike.wazovzky@gmail.com>
2018-06-17 14:37:32 +0300

    Bootstrap styling a-la Laravel.

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="test">
         <h1>Greeting from mw-toolbox!!</h1>
+        <br>
+        <button class="btn btn-xs btn-info">Info</button>
+        <button class="btn btn-sm btn-primary">Primary</button>
+        <button class="btn btn-warning">Warning</button>
+        <button class="btn btn-lg btn-danger">Danger</button>
     </div>
 </template>
 
@@ -16,7 +21,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+@import '../assets/sass/main.scss';
+
 .test h1 {
     color: #40b883;
 }
